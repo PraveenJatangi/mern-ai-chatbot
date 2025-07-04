0f8750e53a1bf521bf930234ec996e10d30f9b0e
Praveen <praveenrams766@gmail.com>
2025-07-04 13:38:29 +0530

    updated deployment with Error Handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 
 //import './App.css'
-import Headers from './components/header' ;
+import Headers from './components/Header' ;
 import{Route,Routes}from 'react-router-dom';
 import Chat from './pages/Chat';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Login from './pages/login';
 import Signin from './pages/Singin';
 import NotFound from './pages/NotFound';
